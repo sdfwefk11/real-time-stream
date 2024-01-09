cf9e1c58b9674ce6667c1badca487b227eb45144
sdfwefk11 <mknhsad3398@naver.com>
2024-01-10 03:43:58 +0900

    Webhooks

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
+import { Logo } from "./_components/logo";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div className="h-full flex items-center justify-center flex-col space-y-6">
+      <Logo />
+      {children}
+    </div>
+  );
 }
