8457905bc158ae3ee294f6875e1010945a31758b
sdfwefk11 <mknhsad3398@naver.com>
2024-01-10 23:48:59 +0900

    test

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -12,6 +12,7 @@ export function Container({ children }: ContainerProps) {
   const { collapsed, onCollapse, onExpand } = useSidebar((state) => state);
   const matches = useMediaQuery("(max-width: 1024px)");
   //브라우저 화면 사이즈가 lg:((max-width: 1024px))이상이면 false 반환하는 함수
+
   useEffect(() => {
     if (matches) {
       onCollapse();
