211c79ab73964b5f6a0c43b5c9a9327a03583ad2
sdfwefk11 <mknhsad3398@naver.com>
2024-02-05 09:53:21 +0900

    Thumbnail animate

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -9,6 +9,10 @@ import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 import { Header, HeaderSkeleton } from "./header";
 import { InfoCard } from "./info-card";
+import { useToggleThumbnail } from "@/store/use-thumbnail-toggle";
+import Image from "next/image";
+import { AlertTriangle, Minimize2 } from "lucide-react";
+import { Button } from "../ui/button";
 
 interface StreamPlayerProps {
   host: Stream_user & { stream: Stream_stream | null };
@@ -19,12 +23,59 @@ interface StreamPlayerProps {
 export function StreamPlayer({ host, stream, isFollowing }: StreamPlayerProps) {
   const { identity, token, name } = useViewerToken(host.id);
   const { collapsed } = useChatSidebar((state) => state);
+  const { isThumbnailToggle, onToggle } = useToggleThumbnail((state) => state);
 
   if (!token || !identity || !name) {
     return <StreamPlayerSkeleton />;
   }
   return (
     <>
+      <div
+        onClick={() => onToggle()}
+        className={cn(
+          "fixed w-full h-full items-center justify-center left-0 top-0 right-0 bottom-0 bg-black opacity-0 transition-all flex pointer-events-none z-50",
+          isThumbnailToggle && "bg-opacity-40 opacity-100 pointer-events-auto"
+        )}
+      >
+        {stream.thumbnailUrl ? (
+          <div
+            className={cn(
+              "absolute aspect-video group transition-all duration-300 w-0 ",
+              isThumbnailToggle &&
+                "w-[440px] h-[300px] md:w-[650px] lg:w-[900px] xl:w-[1100px] md:h-[500px] lg:h-[550px] xl:h-[600px] md:ml-14 lg:ml-0"
+            )}
+          >
+            <Button
+              className="absolute z-50 right-1 top-1 text-muted-foreground group-hover:bg-background"
+              size="icon"
+              variant="ghost"
+            >
+              <Minimize2 className="w-4 h-4" />
+            </Button>
+            <Image
+              fill
+              src={stream.thumbnailUrl}
+              alt={stream.thumbnailUrl}
+              className="rounded-md object-cover border border-[#2D2E35]"
+            />
+          </div>
+        ) : (
+          <div className="absolute w-[440px] h-[300px] aspect-video  md:w-[650px] lg:w-[900px] xl:w-[1100px] md:h-[500px] lg:h-[550px] xl:h-[600px] md:ml-14 lg:ml-0 group transition-all ">
+            <Button
+              className="absolute z-50 right-1 top-1 text-muted-foreground group-hover:bg-background"
+              size="icon"
+              variant="ghost"
+            >
+              <Minimize2 className="w-4 h-4" />
+            </Button>
+            <div className="bg-background shadow w-full h-full flex items-center justify-center flex-col gap-y-3 rounded-md border border-[#2D2E35] text-muted-foreground">
+              <AlertTriangle className="w-10 h-10" />
+              <p className="d font-semibold">등록된 이미지가 없습니다.</p>
+            </div>
+          </div>
+        )}
+      </div>
+
       {collapsed && (
         <div className="hidden lg:block fixed top-[100px] right-2 z-50">
           <ChatToggle />
--- components/stream-player/info-card.tsx
+++ components/stream-player/info-card.tsx
@@ -1,8 +1,11 @@
 "use client";
-import { Pencil } from "lucide-react";
+import { Maximize2, Pencil } from "lucide-react";
 import { Separator } from "../ui/separator";
 import Image from "next/image";
 import { InfoModal } from "./info-modal";
+import { cn } from "@/lib/utils";
+import { useToggleThumbnail } from "@/store/use-thumbnail-toggle";
+import { Button } from "../ui/button";
 
 interface InfoCardProps {
   hostIdentity: string;
@@ -17,10 +20,15 @@ export function InfoCard({
   name,
   thumbnailUrl,
 }: InfoCardProps) {
+  const { onToggle } = useToggleThumbnail((state) => state);
   const hostAsViewer = `host-${hostIdentity}`;
   const isHost = hostAsViewer === viewerIdentity;
 
   if (!isHost) return null;
+
+  const onImageClick = () => {
+    onToggle();
+  };
 
   return (
     <div className="px-4">
@@ -48,7 +56,20 @@ export function InfoCard({
           <div>
             <h3 className="text-sm text-muted-foreground mb-2">Thumbnail</h3>
             {thumbnailUrl && (
-              <div className="relative aspect-video rounded-md overflow-hidden w-[200px] border border-white/10">
+              <div
+                onClick={onImageClick}
+                className={cn(
+                  "relative aspect-video rounded-md overflow-hidden w-[200px] border border-white/10 hover:opacity-90 cursor-pointer group transition-all"
+                )}
+              >
+                <Button
+                  className="absolute z-10 right-0 group-hover:bg-background text-muted-foreground"
+                  size="sm"
+                  type="button"
+                  variant="ghost"
+                >
+                  <Maximize2 className="h-4 w-4" />
+                </Button>
                 <Image
                   fill
                   src={thumbnailUrl}
