5c5eae4a15bdb01d99ef3338390f55a986b51e9e
sdfwefk11 <mknhsad3398@naver.com>
2024-02-17 04:32:38 +0900

    Search

---
+++ app/(browse)/search/_components/results.tsx
@@ -0,0 +1,41 @@
+import { getSearch } from "@/lib/search-service";
+import { ResultCard, ResultCardSkeleton } from "./result-card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface ResultsProps {
+  term?: string;
+}
+
+export async function Results({ term }: ResultsProps) {
+  const data = await getSearch(term);
+  return (
+    <div>
+      <h2 className="text-lg font-semibold mb-4">
+        Results for term &quot;{term}&quot;
+      </h2>
+      {data.length === 0 && (
+        <p className="text-muted-foreground text-sm">
+          No result found. Try searching for something else
+        </p>
+      )}
+      <div className="flex flex-col gap-y-4">
+        {data.map((result) => (
+          <ResultCard key={result.id} data={result} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export function ResultSkeleton() {
+  return (
+    <div>
+      <Skeleton className="h-8 w-[290px] mb-4" />
+      <div className="flex flex-col gap-y-4">
+        {[...Array(4)].map((_, i) => (
+          <ResultCardSkeleton key={i} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/(browse)/search/page.tsx
+++ app/(browse)/search/page.tsx
@@ -1,4 +1,6 @@
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import { ResultSkeleton, Results } from "./_components/results";
 
 interface SearchPageProps {
   searchParams: { term?: string };
@@ -8,5 +10,11 @@ export default function SearchPage({ searchParams }: SearchPageProps) {
   if (!searchParams) {
     redirect("/");
   }
-  return <div className="h-full p-8 max-w-screen-2xl mx-auto">Search Page</div>;
+  return (
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultSkeleton />}>
+        <Results term={searchParams.term} />
+      </Suspense>
+    </div>
+  );
 }
